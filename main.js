9ba445e0b749bc2d1f96292442efd77ce15aed20
yukyiwan <71396331+yukyiwan@users.noreply.github.com>
2021-03-11 16:42:19 -0500

    updated

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
     // console.log("open!");
-    $("#nav").hide();
-    $("#more1").hide();
-    $("#more2").hide();
-    $("#more3").hide();
-    $("#eMore1").hide();
-    $("#eMore2").hide();
-    $("#eMore3").hide();
+    $("#nav").slideUp (800);
+    $("#more1").slideUp (800);
+    $("#more2").slideUp (800);
+    $("#more3").slideUp (800);
+    $("#eMore1").slideUp (800);
+    $("#eMore2").slideUp (800);
+    $("#eMore3").slideUp (800);
 
 
     // $("#header").hover(()=>{
     //     window.location.href = '#header';
-    //     $("#nav").show();
+    //     $("#nav").toggle();
     // });
 
 
     // $("#home").hover(()=>{
-    //     $("#nav").hide();
+    //     $("#nav").slideUp (800);
     // });
 
     // $("#about").hover(()=>{
@@ -36,48 +36,48 @@ $(document).ready(function(){
     // });
 
     $("#work1").hover(()=>{
-        $("#more1").show();
-        $("#more2").hide();
-        $("#more3").hide();
+        $("#more1").toggle();
+        $("#more2").slideUp (800);
+        $("#more3").slideUp (800);
     })
 
     $("#work2").hover(()=>{
-        $("#more2").show();
-        $("#more1").hide();
-        $("#more3").hide();
+        $("#more2").toggle();
+        $("#more1").slideUp (800);
+        $("#more3").slideUp (800);
     })
 
     $("#work3").hover(()=>{
-        $("#more3").show();
-        $("#more1").hide();
-        $("#more2").hide();
+        $("#more3").toggle();
+        $("#more1").slideUp (800);
+        $("#more2").slideUp (800);
     })
 
     $("#edu1").hover(()=>{
-        $("#eMore1").show();
-        $("#eMore2").hide();
-        $("#eMore3").hide();
+        $("#eMore1").toggle();
+        $("#eMore2").slideUp (800);
+        $("#eMore3").slideUp (800);
     })
 
     $("#edu2").hover(()=>{
-        $("#eMore2").show();
-        $("#eMore1").hide();
-        $("#eMore3").hide();
+        $("#eMore2").toggle();
+        $("#eMore1").slideUp (800);
+        $("#eMore3").slideUp (800);
     })
 
     $("#edu3").hover(()=>{
-        $("#eMore3").show();
-        $("#eMore1").hide();
-        $("#eMore2").hide();
+        $("#eMore3").toggle();
+        $("#eMore1").slideUp (800);
+        $("#eMore2").slideUp (800);
     })
 
     // $("body").click(()=>{
-    //     $("#more1").hide();
-    //     $("#more2").hide();
-    //     $("#more3").hide();
-    //     $("#eMore1").hide();
-    //     $("#eMore2").hide();
-    //     $("#eMore3").hide();
+    //     $("#more1").slideUp (800);
+    //     $("#more2").slideUp (800);
+    //     $("#more3").slideUp (800);
+    //     $("#eMore1").slideUp (800);
+    //     $("#eMore2").slideUp (800);
+    //     $("#eMore3").slideUp (800);
 
     // });
 
